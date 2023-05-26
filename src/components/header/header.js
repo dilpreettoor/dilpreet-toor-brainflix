5eeb8c568759431e67c1ffcf5449f1081c609781
dilpreettoor <dilpreet.s.t@gmail.com>
2023-05-26 14:56:40 -0400

    added header features, added mobile+tablet+desktop views

---
+++ src/components/header/header.js
@@ -0,0 +1,50 @@
+import logo from '../../assets/logo/BrainFlix-logo.svg';
+import profile from '../../assets/images/Mohan-muruge.jpg'
+import './header.scss';
+
+function Header() {
+    return (
+        <header className='header-nav'>
+           <img 
+           className='header-nav__logo'
+           src={logo}
+           alt='Brainflix logo' />
+            <div 
+            className='header-nav__wrapper'>
+           <div 
+           className='header-nav__search'>
+                <input 
+                className='header-nav__search-input'
+                placeholder='Search'
+                type='text'>
+                </input>
+
+                <img
+                className='header-nav__profile-photo'
+                src={profile}
+                alt='profile picture'>
+                </img>
+
+                
+
+           </div>
+           <button
+           className='header-nav__upload-btn'>
+            Upload
+           </button>
+
+                <img
+                className='header-nav__profile-tablet'
+                src={profile}
+                alt='profile picture'>
+                </img>
+                </div>
+
+           
+
+        </header>
+    )
+
+}
+
+export default Header;
